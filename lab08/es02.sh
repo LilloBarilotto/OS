1f598e6e52048abd52d584366a76950759c45adc
Sebastiano Guzzone <47942024+LilloBarilotto@users.noreply.github.com>
2021-12-08 11:53:49 +0100

    Add files via upload

--- lab08/es02.sh
+++ lab08/es02.sh
@@ -5,21 +5,27 @@
 #- visualizzi (a video) il numero di caratteri della linea piu' lunga e il numero di righe del file stesso.
 #Si supponga che le righe del file non contengano spazi.
 
+# Alternative version (sostituisci poi $1 con $fname
+# echo "Please write the name of the file: "; read fname;
+ 
+#CONTROLLO DI AVER PASSATO 1 parametro nomefile
+if [ $# -ne 1 ]
+then
+  echo "Give me ren.... one file's name .-."
+  exit 1
+fi
 
-echo "Please write the name of the file: "
-read fname
-
-nline=$(wc -l $fname)
+nline=$(wc -l $1)
 max=0
 
 while read row
 do	
 	tmp=${#row}
-	if $tmp -gt $max
+	if [ $tmp -gt $max ] 
 	then
 		let "max=tmp"
 	fi
-done <$fname
+done <$1
 
 echo "NumberLIne= $nline, MaxNumberChar= $max"
 
