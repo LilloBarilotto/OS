4066dbeeededdb3384a28a78075e63b3885f3fa7
Sebastiano Guzzone <47942024+LilloBarilotto@users.noreply.github.com>
2021-12-08 12:29:02 +0100

    Add files via upload

--- lab08/es03.sh
+++ lab08/es03.sh
@@ -2,14 +2,16 @@
 
 # directory name is in $1
 
-if $# != 1
-	echo "Oh come on bro..."
-exit 1
+if [ $# != 1 ]
+then
+	echo "Oh come on bro... Give me DIR"
+	exit 1
+fi
 
 for var in $(ls $1)
 do
 	lowString=$(echo $var | awk '{print tolower($0)}')
-	mv $var $lowString
+	mv $1/${var} $1/${lowString}
 done
 
 exit 0
